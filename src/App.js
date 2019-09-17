bd140e7f35583907168dd82c1257d76ba1e7c875
mrsalvini <dsgmcosta@gmail.com>
2019-09-17 23:36:19 +0200

    basic website with name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, ThemeProvider, Styled } from 'theme-ui'
+import theme from './theme'
+
+// does not currently work with MDX v1
+// import { importMDX } from 'mdx.macro'
+// const Demo = importMDX.sync('./demo.mdx')
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div
+        sx={{
+          height: '100%',
+        }}
+      >
+        <Styled.h1>damiennaylor.com</Styled.h1>
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/theme.js
@@ -0,0 +1,30 @@
+export default {
+  colors: {
+    text: '#000',
+    background: '#000',
+    primary: '#ccc',
+    secondary: '#609',
+  },
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'inherit',
+  },
+  styles: {
+    body: {
+      height: '100vh',
+      width: '100vw',
+    },
+    h1: {
+      fontSize: [4, 5, 6],
+      color: 'primary',
+    },
+    a: {
+      color: 'primary',
+      textDecoration: 'none',
+      ':hover': {
+        color: 'secondary',
+        textDecoration: 'underline',
+      },
+    },
+  },
+}
